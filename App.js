3da00eab71478d1d4333ab1f3a2f8ffcfa1d308d
justan-kj <justintantj@gmail.com>
2023-06-02 18:08:36 +0800

    assignment

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, View, StyleSheet } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { Picker } from "@react-native-picker/picker";
+
+const searchEngines = [
+  { label: "Google", value: "https://www.google.com" },
+  { label: "Bing", value: "https://www.bing.com" },
+  { label: "DuckDuckGo", value: "https://duckduckgo.com" },
+  { label: "Yahoo", value: "https://yahoo.com" },
+];
 
 export default function App() {
+  const [selectedSite, setSelectedSite] = useState(searchEngines[0].value);
+
+  const _handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync(selectedSite);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Pick a search engine:</Text>
+      <Picker
+        selectedValue={selectedSite}
+        onValueChange={(itemValue) => setSelectedSite(itemValue)}
+        style={styles.picker}
+      >
+        {searchEngines.map((website) => (
+          <Picker.Item
+            key={website.value}
+            label={website.label}
+            value={website.value}
+          />
+        ))}
+      </Picker>
+      <Button title="Open WebBrowser" onPress={_handlePressButtonAsync} />
     </View>
   );
 }
@@ -13,8 +40,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  picker: {
+    width: "50%",
+    marginBottom: 16,
   },
 });
